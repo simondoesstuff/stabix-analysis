simondoesstuff/stabix-analysis

commit 1856851cdc5921bb7e99ad078f40989b11be68c8

hdf5 py missing pval_col check

scripts/python/sqlite_hdf5/hdf5_query.py
@@ -42,11 +42,21 @@ def build_gwas_hdf5(tsv_path, hdf5_path, pval_col, chunksize=10000):
                         col: 6 for col in group.select_dtypes('string').columns})
 
 
+issue_chroms = set()
+
+
 def query_one(store, chrom, start, end, pval_col, pval):
     chrom = str(chrom).strip().upper()
     key = f"/chr{chrom}"
 
     if key not in store:
+        return pd.DataFrame()
+
+    table_columns = store.get_storer(key).table.colnames
+    if pval_col not in table_columns:
+        if chrom not in issue_chroms:
+            print(f" ! {pval_col} column not found in: {chrom}")
+            issue_chroms.add(chrom)
         return pd.DataFrame()
 
     query = (

commit c30aab4a1edd646547b96473f4e1e429e727ee59

fixed major issue caused by commit 1856851c

scripts/python/sqlite_hdf5/hdf5_query.py
@@ -62,7 +62,7 @@ def query_one(store, chrom, start, end, pval_col, pval):
     query = (
         f"pos >= {start} & "
         f"pos <= {end} & "
-        f"{pval_col}")
+        f"{pval_col} {pval}")
 
     return store.select(
         key,